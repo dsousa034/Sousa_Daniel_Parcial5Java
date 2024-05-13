933916e77511fd6191b9bec39aa285b73cd658d7
dsousa034 <150059798+dsousa034@users.noreply.github.com>
2024-05-13 15:41:33 +0200

    Restaurante Grupo

--- PatronesEstructuracion/GrupoRestaurantes.java
+++ PatronesEstructuracion/GrupoRestaurantes.java
@@ -8,16 +8,16 @@ public class GrupoRestaurantes extends RestauranteComposite{
 
     @Override
     public void servirCliente() {
-
+        System.out.println("Entrega del servicio en grupo de restaurantes");
     }
 
     @Override
     public void mostrarMenu() {
-
+        System.out.println("Menu del grupo de restaurantes");
     }
 
     @Override
     public void realizarReserva() {
-
+        System.out.println("Reserva en grupo de restaurantes realizada");
     }
 }
--- PatronesEstructuracion/RestauranteComposite.java
+++ PatronesEstructuracion/RestauranteComposite.java
@@ -3,5 +3,4 @@ package PatronesEstructuracion;
 public abstract class RestauranteComposite implements Restaurante {
     public abstract void mostrarMenu();
     public abstract void realizarReserva();
-    // other methods...
 }
