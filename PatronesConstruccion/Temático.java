d41d05a35901a5e4945ac7162e137dfb4f5d26e0
dsousa034 <150059798+dsousa034@users.noreply.github.com>
2024-05-13 15:08:04 +0200

    Restaurante Tematico

---
+++ PatronesConstruccion/Temático.java
@@ -0,0 +1,8 @@
+package PatronesConstruccion;
+
+public class Temático implements Restaurante{
+    @Override
+    public void servirCliente() {
+        System.out.println("Sirviendo cliente en restaurante temático");
+    }
+}
