4fe2628c007e76ee00dc3c61a40c9b5251f06f39
dsousa034 <150059798+dsousa034@users.noreply.github.com>
2024-05-13 15:41:41 +0200

    Restaurante Individual

--- PatronesEstructuracion/RestauranteIndividual.java
+++ PatronesEstructuracion/RestauranteIndividual.java
@@ -3,16 +3,17 @@ package PatronesEstructuracion;
 public class RestauranteIndividual extends RestauranteComposite{
     @Override
     public void servirCliente() {
+        System.out.println("Entrega individual en restaurante");
 
     }
 
     @Override
     public void mostrarMenu() {
-
+        System.out.println("Menu intividual del restaurante");
     }
 
     @Override
     public void realizarReserva() {
-
+        System.out.println("Reserva individual realizada en restaurante");
     }
 }
