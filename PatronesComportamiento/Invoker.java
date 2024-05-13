ee87b1801ba4e0b3e28e86064fca68473cebc75c
dsousa034 <150059798+dsousa034@users.noreply.github.com>
2024-05-13 16:00:22 +0200

    Invoker Command

---
+++ PatronesComportamiento/Invoker.java
@@ -0,0 +1,19 @@
+package PatronesComportamiento;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Invoker {
+    private List<Command> command = new ArrayList<>();
+
+    public void addCommand(Command command){
+        this.command.add(command);
+    }
+
+    public void executeCommands(){
+        for (Command command : this.command) {
+            command.execute();
+        }
+    }
+
+}
