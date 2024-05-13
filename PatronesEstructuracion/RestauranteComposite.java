ecde3871ba630b7c8ceb59a0ec7949dd71ab6c25
dsousa034 <150059798+dsousa034@users.noreply.github.com>
2024-05-13 15:32:15 +0200

    Restaurante Composite

---
+++ PatronesEstructuracion/RestauranteComposite.java
@@ -0,0 +1,7 @@
+package PatronesEstructuracion;
+
+public abstract class RestauranteComposite implements Restaurante {
+    public abstract void mostrarMenu();
+    public abstract void realizarReserva();
+    // other methods...
+}
