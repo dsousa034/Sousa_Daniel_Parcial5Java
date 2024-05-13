e13780461979aef918d6d4e9b85ec9bdf0a2aa7b
dsousa034 <150059798+dsousa034@users.noreply.github.com>
2024-05-13 15:36:07 +0200

    Restaurante Individual

---
+++ PatronesEstructuracion/RestauranteIndividual.java
@@ -0,0 +1,18 @@
+package PatronesEstructuracion;
+
+public class RestauranteIndividual extends RestauranteComposite{
+    @Override
+    public void servirCliente() {
+
+    }
+
+    @Override
+    public void mostrarMenu() {
+
+    }
+
+    @Override
+    public void realizarReserva() {
+
+    }
+}
