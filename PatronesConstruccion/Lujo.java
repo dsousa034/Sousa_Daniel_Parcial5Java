377c7c916a71c27d7ad22b168edd15edc818bed4
dsousa034 <150059798+dsousa034@users.noreply.github.com>
2024-05-13 15:08:17 +0200

    Restaurante de Lujo

---
+++ PatronesConstruccion/Lujo.java
@@ -0,0 +1,8 @@
+package PatronesConstruccion;
+
+public class Lujo implements Restaurante{
+    @Override
+    public void servirCliente() {
+        System.out.println("Sirviendo cliente en restaurante de lujo");
+    }
+}
