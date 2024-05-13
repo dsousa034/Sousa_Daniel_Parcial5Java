237732e71bcd67a635080a5929af2c4466aae686
dsousa034 <150059798+dsousa034@users.noreply.github.com>
2024-05-13 15:36:22 +0200

    Restaurante Grupo

---
+++ PatronesEstructuracion/GrupoRestaurantes.java
@@ -0,0 +1,23 @@
+package PatronesEstructuracion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GrupoRestaurantes extends RestauranteComposite{
+    List<RestauranteIndividual> listaRestaurantes = new ArrayList<>();
+
+    @Override
+    public void servirCliente() {
+
+    }
+
+    @Override
+    public void mostrarMenu() {
+
+    }
+
+    @Override
+    public void realizarReserva() {
+
+    }
+}
